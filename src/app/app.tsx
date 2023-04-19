f44a3a35612f0e3b9b19c068c03fc5a1aaa199f3
temchik00 <temchik0020@gmail.com>
2023-04-19 16:10:36 +0300

    init

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="practice-works-frontend" />
-
-      <div />
+      <div>Hello world</div>
     </>
   );
 }
